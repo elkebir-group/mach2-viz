4f172827acc5da85ed005bbe2f69c1ca2ee4a621
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-02-09 19:43:11 -0600

    About page and basic home page

---
+++ src/About.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from "react-dom";
+
+function About() {
+    return (
+        <div className='about'>
+            <p className='backarrow'><b>&#8592; back</b></p>
+            <div className='panel'>
+                <h1 className='abouttitle'>Enter MACHINA-Viz</h1>
+                <p>MACHINA-Viz is a personalized medical tool to visualize cancer metastasis. Log in or create
+    an account to upload patient metadata. Data is formatted as a folder containing:</p>
+                <ul>
+                    <li><p>A JSON file designating the role of each file</p></li>
+                    <li><p>Clonal tree output(s)</p></li>
+                    <li><p>(Optional) .tsv showing variant allele frequencies</p></li>
+                </ul>
+                <p>Click on the processed input entry to view the corresponding visualization. Follow the
+    directions noted in the visualization window.</p>
+                <p><b>Questions? Contact</b></p>
+                <ul>
+                    <li><p>Akul Joshi (<a href="mailto:akulj2@illinois.edu">akulj2@illinois.edu</a>)</p></li>
+                    <li><p>Vikram Ramavarapu (<a href="mailto:vikramr2@illinois.edu">vikramr2@illinois.edu</a>)</p></li>
+                    <li><p>Mohammed El-Kebir (<a href="mailto:melkebir@illinois.edu">melkebir@illinois.edu</a>)</p></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './About.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="float-container">
+        <div className="float-child">
+          <div className="homedir"><b>MACHINA-Viz</b></div>
+        </div>
+        <div class="float-child">
+          <div className="aboutdir">About</div>
+        </div>
+      </div>
       </header>
+      <About/>
+      {/*<div id="addnew" className="dot"><h1 className="plus">+</h1></div>*/}
     </div>
   );
 }
