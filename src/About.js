e5cd412af3819f06fbe7c9a330d24211732acb7e
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-02-10 21:02:43 -0600

    File upload

--- src/About.js
+++ src/About.js
@@ -1,13 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from "react-dom";
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-
 function About() {
     return (
         <div className='about'>
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './About.js'
+import Viz from './Viz.js'
+import { compressUrlSafe } from './lzma-url.js'
 
 import {
   BrowserRouter as Router,
@@ -9,7 +11,49 @@ import {
   Link
 } from "react-router-dom";
 
+// LZW-compress a string
+function lzw_encode(s) {
+  var dict = {};
+  var data = (s + "").split("");
+  var out = [];
+  var currChar;
+  var phrase = data[0];
+  var code = 256;
+  for (var i=1; i<data.length; i++) {
+      currChar=data[i];
+      if (dict[phrase + currChar] != null) {
+          phrase += currChar;
+      }
+      else {
+          out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+          dict[phrase + currChar] = code;
+          code++;
+          phrase=currChar;
+      }
+  }
+  out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+  for (var i=0; i<out.length; i++) {
+      out[i] = String.fromCharCode(out[i]);
+  }
+  return out.join("");
+}
+
 function App() {
+  let handleChange = (e) => {
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    let json_contents;
+
+    reader.onload = function(readerEvt) {
+      let jsonString = readerEvt.target.result;
+      json_contents = jsonString;
+      console.log(json_contents);
+      window.location = window.location + `viz?data=${compressUrlSafe(json_contents)}`
+    }
+    // Read the file as a text
+    reader.readAsText(file);
+  }
+
   return (
     <div className="App">
       <Router>
@@ -29,17 +73,17 @@ function App() {
             <About/>
           </Route>
           <Route path="/viz">
-            (
-              const queryParams = 
-            )
+            <p>Successful Upload :)</p>
+            <Viz/>
           </Route>
           <Route exact path="/">
             <input 
               type="file"
               id="json_upload" 
               name="json_upload"
-              accept="image/*"/>
-            <label for="json_upload"><div id="addnew" className="dot"><h1 className="plus">+</h1></div></label>
+              accept="application/JSON"
+              onChange={handleChange}/>
+            <label for="json_upload"><div id="addnew" className="dot"><h1 className="plus">+</h1><span class="tooltiptext"><b>Upload Patient JSON</b></span></div></label>
           </Route>
         </Switch>
       </Router>
