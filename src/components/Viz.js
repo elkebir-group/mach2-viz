766032b67d361c9a51bd89fd292eeac6b7de8b84
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-02-16 11:52:44 -0600

    basic viz page

--- src/components/Viz.js
+++ src/components/Viz.js
@@ -23,31 +23,36 @@ function Viz() {
 
     return (
       <div className="viz">
-        <div className="panel instr">
-          <p><b>Instructions:</b> Drag over the model in the right to 
-          rotate the figure around. Anatomic sites will be 
-          highlighted according to the color scheme in the 
-          legend to the right. To view anatomic sites in 
-          different systems, click the tabs in the right.</p>
+        <div className="leftpanel">
+          <div className="panel instr">
+            <p><b>Instructions:</b> Drag over the model in the right to 
+            rotate the figure around. Anatomic sites will be 
+            highlighted according to the color scheme in the 
+            legend to the right. To view anatomic sites in 
+            different systems, click the tabs in the right.</p>
 
-          <p>Click the tabs below to switch between viewing the 
-          clonal tree and migration graph. Multiple labelings
-          and trees may be inferred if a reported labeling is
-          not given. In which case there will be a drop-down to
-          select a potential solution.</p>
-        </div>
-        <div className="panel inputs">
-          <nav className="togglemenu">
-            <p className="jsonheader" onClick={toggleClick}><b>{toggle}</b></p>
-            <ul className="panel jsonview" style={jsonPanel}>
-              <p>{fileContents}</p>
+            <p>Click the tabs below to switch between viewing the 
+            clonal tree and migration graph. Multiple labelings
+            and trees may be inferred if a reported labeling is
+            not given. In which case there will be a drop-down to
+            select a potential solution.</p>
+          </div>
+          <div className="panel inputs">
+            <nav className="togglemenu">
+              <p className="jsonheader" onClick={toggleClick}><b>{toggle}</b></p>
+              <ul className="panel jsonview" style={jsonPanel}>
+                <p>{fileContents}</p>
+              </ul>
+            </nav>
+            <p><b>Patient:</b> {data["name"]}</p>
+            <p><b>Metastasis Locations:</b></p>
+            <ul>
+              {locations.map(l => <li><p>{l}</p></li>)}
             </ul>
-          </nav>
-          <p><b>Patient:</b> {data["name"]}</p>
-          <p><b>Metastasis Locations:</b></p>
-          <ul>
-            {locations.map(l => <li><p>{l}</p></li>)}
-          </ul>
+          </div>
+        </div>
+        <div className="panel info">
+          <h3><b>{data["name"]}</b></h3>
         </div>
       </div>
     )
