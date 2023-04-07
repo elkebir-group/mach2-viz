12d670cf35bdce64e7be4acc69f169b39375e145
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-04-07 13:46:45 -0500

    downloading fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,38 +11,18 @@ import {
 import URLs from "../default_url.json";
 import download from "../assets/download.png";
 
-import A7json from "../samples/A7/A7.json";
-import A10json from "../samples/A10/A10.json";
-import A22json from "../samples/A22/A22.json";
-import A29json from "../samples/A29/A29.json";
-import A31json from "../samples/A31/A31.json";
-import A32json from "../samples/A32/A32.json";
-
-import patient1json from "../samples/patient1/patient1.json";
-import patient2json from "../samples/patient2/patient2.json";
-import patient3json from "../samples/patient3/patient3.json";
-import patient4json from "../samples/patient4/patient4.json";
-import patient7json from "../samples/patient7/patient7.json";
-import patient9json from "../samples/patient9/patient9.json";
-import patient10json from "../samples/patient10/patient10.json";
+import axios from 'axios'
+import fileDownload from 'js-file-download'
 
 function Home() {
-  console.log(A7json)
-    var file_dict = {
-      "A7": A7json,
-      "A10": A10json,
-      "A22": A22json,
-      "A29": A29json,
-      "A31": A31json,
-      "A32": A32json,
-      "patient1": patient1json,
-      "patient2": patient2json,
-      "patient3": patient3json,
-      "patient4": patient4json,
-      "patient7": patient7json,
-      "patient9": patient9json,
-      "patient10": patient10json,
-    } 
+    let handleDownload = (url, filename) => {
+      axios.get(url, {
+        responseType: 'blob',
+      })
+      .then((res) => {
+        fileDownload(res.data, filename)
+      })
+    }
 
     var default_patients = ["A7", "A10", "A22", "A29", "A31", "A32", "patient1", "patient2", "patient3", "patient4", "patient7", "patient9", "patient10"]
     var default_dirs = ["hoadley_2016", "gundem_2015", "gundem_2015", "gundem_2015", "gundem_2015", "gundem_2015", "mcpherson_2016", "mcpherson_2016", "mcpherson_2016", "mcpherson_2016", "mcpherson_2016", "mcpherson_2016", "mcpherson_2016"]
@@ -72,7 +52,12 @@ function Home() {
                 </div>
               </li>
             </Link>
-            <a href={`https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/vikramr2/machina-viz/blob/main/src/samples/${current_patient}/${current_patient}.json`} target="_blank" download><img className='rightli' src={download}></img></a>
+            <img 
+              className='rightli' 
+              src={download}
+              onClick={() => handleDownload(`https://raw.githubusercontent.com/vikramr2/machina-viz/main/src/samples/${default_patients[i]}/${default_patients[i]}.json`, `${default_patients[i]}.json`)}
+            >
+            </img>
           </div>
         </div>)
         which_color = !which_color;
