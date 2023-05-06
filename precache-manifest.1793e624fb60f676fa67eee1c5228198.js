a4d150fb31ff8eaaf5abac859affabea1c5106f2
Akul Joshi <akulj2@illinois.edu>
2023-05-06 12:42:08 -0500

    Updates

---
+++ precache-manifest.1793e624fb60f676fa67eee1c5228198.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a979aa7699202e860324",
+    "url": "/static/css/main.c13cac8a.chunk.css"
+  },
+  {
+    "revision": "a979aa7699202e860324",
+    "url": "/static/js/main.a979aa76.chunk.js"
+  },
+  {
+    "revision": "b9699fdd249ffdecb04e",
+    "url": "/static/js/1.b9699fdd.chunk.js"
+  },
+  {
+    "revision": "3a6e89dac2060ee38975",
+    "url": "/static/js/2.3a6e89da.chunk.js"
+  },
+  {
+    "revision": "d045ba4c1770e68b8d83",
+    "url": "/static/js/runtime~main.d045ba4c.js"
+  },
+  {
+    "revision": "0c0c2213b2e21efee6c5d203ce6648a3",
+    "url": "/static/media/map.0c0c2213.jpeg"
+  },
+  {
+    "revision": "97812ed68e010687c3e75819891d5746",
+    "url": "/static/media/logo.97812ed6.png"
+  },
+  {
+    "revision": "80b88740f6df1df857f10a09970fd63b",
+    "url": "/static/media/settings-gear.80b88740.svg"
+  },
+  {
+    "revision": "90b2cac68b3e73567c3e8e84a28b3955",
+    "url": "/index.html"
+  }
+];
