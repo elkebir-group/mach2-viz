dc4775d5788fcb47e3dde3f1821f78a8bff161c3
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-05-06 12:52:20 -0500

    Updates

---
+++ precache-manifest.b643ab4acb2d5a0338067e9870bb2f9a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "22fee90b534995c5b943",
+    "url": "/machina-viz/static/css/main.293d9641.chunk.css"
+  },
+  {
+    "revision": "22fee90b534995c5b943",
+    "url": "/machina-viz/static/js/main.22fee90b.chunk.js"
+  },
+  {
+    "revision": "b9699fdd249ffdecb04e",
+    "url": "/machina-viz/static/js/1.b9699fdd.chunk.js"
+  },
+  {
+    "revision": "096fec23b6d272bd933d",
+    "url": "/machina-viz/static/js/2.096fec23.chunk.js"
+  },
+  {
+    "revision": "7aee411d69c8698003f1",
+    "url": "/machina-viz/static/js/runtime~main.7aee411d.js"
+  },
+  {
+    "revision": "0c0c2213b2e21efee6c5d203ce6648a3",
+    "url": "/machina-viz/static/media/map.0c0c2213.jpeg"
+  },
+  {
+    "revision": "97812ed68e010687c3e75819891d5746",
+    "url": "/machina-viz/static/media/logo.97812ed6.png"
+  },
+  {
+    "revision": "80b88740f6df1df857f10a09970fd63b",
+    "url": "/machina-viz/static/media/settings-gear.80b88740.svg"
+  },
+  {
+    "revision": "79c1a91846e64b6b30e46271b4852b35",
+    "url": "/machina-viz/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1793e624fb60f676fa67eee1c5228198.js"
+  "/machina-viz/precache-manifest.b643ab4acb2d5a0338067e9870bb2f9a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/machina-viz/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
