94b7874c756d696bc87dfaf01e3857069256ccc7
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-05-29 18:58:33 -0500

    extend detachment support to other browsers

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import typer
 import json
 import sys
+import builtins
 
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def main(
     input: str = typer.Option(..., "--input", "-i"),
@@ -14,17 +16,22 @@ def main(
     # Select driver based on the argument
     if browser == "chrome":
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_experimental_option("detach", True)
+        chrome_options.add_experimental_option("detach", True)      # Keep browser open when script closes
         driver = webdriver.Chrome(options=chrome_options)
     elif browser == "firefox":
-        driver = webdriver.Firefox()
+        firefox_options = webdriver.FirefoxOptions()
+        firefox_options.add_experimental_option("detach", True)
+        driver = webdriver.Firefox(options=firefox_options)
     elif browser == "safari":
         driver = webdriver.Safari()
     elif browser == "edge":
-
-        driver = webdriver.Edge()
+        edge_options = webdriver.EdgeOptions()
+        edge_options.add_experimental_option("detach", True)
+        driver = webdriver.Edge(options=edge_options)
     elif browser == "explorer" or browser == "ie":
-        driver = webdriver.Ie()
+        ie_options = webdriver.IeOptions()
+        ie_options.add_experimental_option("detach", True)
+        driver = webdriver.Ie(options=ie_options)
     else:
         raise ValueError("Invalid browser argument. Must choose from: chrome, firefox, safari, edge, explorer or ie")
     
@@ -40,6 +47,10 @@ def main(
     driver.get('https://elkebir-group.github.io/mach2-viz')
     driver.execute_script(f"window.sessionStorage.setItem('json_data', '{json_string}')")
     driver.get(f'https://elkebir-group.github.io/mach2-viz/#/mach2-viz/viz?labeling={labels[0]}')
+
+    if browser == 'safari':
+        print("Press ENTER to continue...")
+        builtins.input()
 
 if __name__ == "__main__":
     typer.run(main)
