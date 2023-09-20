e7fae1c4b0ccafb9f23c942b5fcb65d7ce9b74a2
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-09-20 13:26:38 -0500

    Updates

---
+++ precache-manifest.652ae8d1bde6e9efd4157d64ec80c1b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c765c16d9c9211c49a7",
+    "url": "/mach2-viz/static/css/main.f5a66a84.chunk.css"
+  },
+  {
+    "revision": "9c765c16d9c9211c49a7",
+    "url": "/mach2-viz/static/js/main.9c765c16.chunk.js"
+  },
+  {
+    "revision": "84db0e82a2ff27b56ba4",
+    "url": "/mach2-viz/static/js/1.84db0e82.chunk.js"
+  },
+  {
+    "revision": "903e3b82c0e8934f3ca5",
+    "url": "/mach2-viz/static/js/2.903e3b82.chunk.js"
+  },
+  {
+    "revision": "26043361e703b0bcfdd3",
+    "url": "/mach2-viz/static/js/runtime~main.26043361.js"
+  },
+  {
+    "revision": "802f465ba478ae1a943d3cf4abe5c70d",
+    "url": "/mach2-viz/static/media/sample_page.802f465b.png"
+  },
+  {
+    "revision": "e5d128666982eeeabae9cc97590309ce",
+    "url": "/mach2-viz/static/media/cursor8.e5d12866.cur"
+  },
+  {
+    "revision": "97a55c954a18d2daae22c5f9114794d5",
+    "url": "/mach2-viz/static/media/lucon.97a55c95.ttf"
+  },
+  {
+    "revision": "e64b037d00c1a00c91af4b4558e09c0a",
+    "url": "/mach2-viz/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mach2-viz/precache-manifest.89f681fb6bc2e7249b193ece51fe1d2d.js"
+  "/mach2-viz/precache-manifest.652ae8d1bde6e9efd4157d64ec80c1b2.js"
 );
 
 workbox.clientsClaim();
