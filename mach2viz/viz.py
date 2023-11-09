9dab1ccee7e06dd5df49df20d893927c15cd56be
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-11-08 18:43:28 -0600

    server gateway for json file

--- mach2viz/viz.py
+++ mach2viz/viz.py
@@ -1,10 +1,35 @@
-from flask import Flask  # Import flask
+'''
+@author Vikram Ramavarapu, Nishant Sheikh
 
-app = Flask(__name__, static_url_path='/mach2-viz/')  # Setup the flask app by creating an instance of Flask
+Server side for starting viz via commandline argument
+'''
+
+import json
+
+from flask import Flask, jsonify  # Import flask
+
+
+JSON_FILE = '/Users/vikram/Documents/Research/El-Kebir/mach2-viz/mach2viz-client/src/samples/A1/A1.json'
+
+# Setup the flask app by creating an instance of Flask
+app = Flask(__name__, static_url_path='/mach2-viz/')
 
 @app.route('/')
-def home():  # At the same home function as before
+def home():
+    ''' Home route to open the page '''
+
     return app.send_static_file('index.html')  # Return index.html from the static folder
+
+@app.route('/json')
+def send_json():
+    ''' API Gateway to send json data '''
+
+    # Open the JSON file
+    with open(JSON_FILE, 'r', encoding='utf-8') as file:
+        # Load the JSON data
+        json_data = json.load(file)
+
+    return jsonify({"data": json.dumps(json_data)})
 
 if __name__ == '__main__':  # If the script that was run is this script (we have not been imported)
     app.run()  # Start the server
